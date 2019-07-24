c72d66955bce252f4147c8ceb3d9aa0a34eb4e0f
chenchaojie <1262137731@qq.com>
2019-07-24 08:04:15 +0800

    added 剑指offer (重复数组问题1)

---
+++ offer/interview_3_repeat_array.go
@@ -0,0 +1,16 @@
+package offer
+
+func repeat_array(dst []int) int {
+	if dst == nil || len(dst) == 0 {
+		return -1
+	}
+
+	n := len(dst)
+	for i := 0; i < n; i++ {
+		// todo
+		if dst[i] == i {
+
+		}
+	}
+
+}
