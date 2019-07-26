6ed054f9963019d9d371e6b7fbad52007b5734fc
chenchaojie <1262137731@qq.com>
2019-07-26 08:01:22 +0800

    done 剑指offer 习题3 重复数组

--- offer/interview_3_repeat_array.go
+++ offer/interview_3_repeat_array.go
@@ -1,5 +1,12 @@
 package offer
 
+/*
+题目一: 找出数组中重复的数字
+	在一个长度为n的数组里的所有数字都在0~n-1的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，
+	也不知道每个数字重复几次，请找出数组中任意一个重复的数字
+
+*/
+
 func repeat_array(dst []int) int {
 	if dst == nil || len(dst) == 0 {
 		return -1
@@ -7,10 +14,14 @@ func repeat_array(dst []int) int {
 
 	n := len(dst)
 	for i := 0; i < n; i++ {
-		// todo
-		if dst[i] == i {
-
+		for dst[i] != i {
+			if dst[dst[i]] == dst[i] {
+				return dst[i]
+			}
+			tmp := dst[i]
+			dst[i] = dst[dst[i]]
+			dst[dst[i]] = tmp
 		}
 	}
-
+	return -1
 }
